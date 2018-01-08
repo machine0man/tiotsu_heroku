4f9c534dc5ba09ff42996e6d06e5ff3d3b479e07
Shanbir Singh <shanbirsingh@gmail.com>
2018-01-09 03:02:25 +0530

    Update newwork.py

--- newwork.py
+++ newwork.py
@@ -33,7 +33,7 @@ def GetGeolocationAndAddDatasetFeature():
     Username = request.form['Username']
     #feature = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'properties': {'MyHouse': 'Towntest'}, 'geometry': {'coordinates': [Geolocation], 'type': 'Point'}, 'id': 'feature-id'}]}
     feature = {'type': 'Feature', 'id': Username, 'properties': {'name': 'Towntest'},'geometry': {Geolocation}}
-    datasets.update_feature('cjbwkjod422u233nx1xp8ltzr','feature-id',feature)
+    datasets.update_feature('cjbwkjod422u233nx1xp8ltzr',Username,feature)
     return 'OK'
  
 if __name__ == '__main__':
