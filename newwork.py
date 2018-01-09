2959071c5e5db09aa649f2dca7d8be1b92e8440d
Shanbir Singh <shanbirsingh@gmail.com>
2018-01-10 03:33:06 +0530

    Update newwork.py

--- newwork.py
+++ newwork.py
@@ -45,7 +45,7 @@ def GetGeolocationAndAddDatasetFeature():
     if upload_resp.status_code == 422:
         for i in range(5):
             sleep(5)
-            with open('data.geojson', 'r',encoding='utf-8') as src:
+            with open('data.geojson', 'rb') as src:
                 upload_resp = service.upload(src, 'pappussp1.data')
             if upload_resp.status_code != 422:
                 break
