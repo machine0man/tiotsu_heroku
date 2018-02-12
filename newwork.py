6b26c123d74f97290796607b9324b8ac1bc83cea
Shanbir Singh <shanbirsingh@gmail.com>
2018-02-12 18:56:03 +0530

    Update newwork.py

--- newwork.py
+++ newwork.py
@@ -127,7 +127,7 @@ def GetGeolocationAndAddDatasetFeature():
         db.session.commit()
     #feature = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'properties': {'MyHouse': 'Towntest'}, 'geometry': {'coordinates': [Geolocation], 'type': 'Point'}, 'id': 'feature-id'}]}
     #feature = {"type": "Feature", "id": Username, "properties": {'name": "Towntest"},"geometry": {Geolocation}}
-    feature=eval(Geolocation)
+    feature=Geolocation
     print("DatasetNotUploaded")
     datasets.update_feature('cjbwkjod422u233nx1xp8ltzr',email,feature)
     rint("DatasetUploaded")
