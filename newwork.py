d72c655db421f89cf5f46b240986629f68f47130
Shanbir Singh <shanbirsingh@gmail.com>
2018-02-13 22:39:25 +0530

    Update newwork.py

--- newwork.py
+++ newwork.py
@@ -17,11 +17,6 @@ db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vrtvapidqltuni:abf33013012ea480de6c1d50bc4230d6218296846ba8a5bd4b44c80fa7325859@ec2-54-227-250-33.compute-1.amazonaws.com:5432/d9p6o27d01ao21'
 app.config['SECRET_KEY'] = 'oh_so_secret'
-
-@app.before_request
-def session_management():
-    # make the session last indefinitely until it is cleared
-    session.permanent = True
 def getdatafromtiotsu():
     firstname=request.form['username']
     email=request.form['mymail']
@@ -77,28 +72,26 @@ def createanduploaddata():
     return "OK"
 
 
-#@app.route('/tiotsudatasend',methods=['GET'])
+@app.route('/tiotsudatasend',methods=['GET'])
 def senddatatotiotsu():
-    if (request.method=='GET'):
-        mymail=session['emailattacksession']
-        update_this = tiotsu_users.query.filter_by(email = mymail).first()
-        if(update_this):
-            auraattack=update_this.aura
-            firstnameattack=update_this.firstname
-            yunkattack=update_this.yunk
-            session.pop('emailattacksession', None)
-            session.clear()
-        tiotsudata=auraattack+"\r\n"+firstnameattack+"\r\n"+yunkattack
-        return tiotsudata
+    mymail=session['emailattacksession']
+    update_this = tiotsu_users.query.filter_by(email = mymail).first()
+    if(update_this):
+        auraattack=update_this.aura
+        firstnameattack=update_this.firstname
+        yunkattack=update_this.yunk
+        session.pop('emailattacksession', None) 
+    tiotsudata=auraattack+"\r\n"+firstnameattack+"\r\n"+yunkattack
+    return tiotsudata
 
-@app.route('/tiotsudataget',methods=['GET','POST'])
+@app.route('/tiotsudataget',methods=['POST'])
 def getattackdatafromtiotsu():
     if (request.method == "POST"):
-        session.clear() 
+        session.pop('emailattacksession', None) 
         emailattack=request.form['emailattack']
         session['emailattacksession']=emailattack
         print(session['emailattacksession'])
-    return senddatatotiotsu()
+    return "OK"
 
 @app.route('/alreadyuser',methods=['POST'])
 def alreadyuserupdatedata():
